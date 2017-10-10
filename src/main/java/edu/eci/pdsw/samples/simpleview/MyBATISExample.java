7f4e199614081c3d54e271ccbab6bce84c798ada
cesareduardo <cesareduardo824@gmail.com>
2017-10-09 23:54:49 -0500

    problemas con el .comit

--- src/main/java/edu/eci/pdsw/samples/simpleview/MyBATISExample.java
+++ src/main/java/edu/eci/pdsw/samples/simpleview/MyBATISExample.java
@@ -13,15 +13,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Date;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import static java.awt.Event.INSERT;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.apache.ibatis.annotations.Param;
+
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -62,41 +58,27 @@ public class MyBATISExample {
      * @throws SQLException 
      */
     public static void main(String args[]) throws SQLException {
-        try{
-            String url="jdbc:mysql://desarrollo.is.escuelaing.edu.co:3306/bdprueba";
-            String driver="com.mysql.jdbc.Driver";
-            String user="bdprueba";
-            String pwd="bdprueba";
-                        
-            Class.forName(driver);
-            Connection conn=DriverManager.getConnection(url,user,pwd);
-            conn.setAutoCommit(false);
-            
+        
+       
             
         
-            Consulta con=new Consulta();
-            Eps ep=new Eps();        
-            Paciente pa= new Paciente(9999,"CC","Oscar", Date.valueOf("2016-12-12"),ep);
-            SqlSessionFactory sessionfact = getSqlSessionFactory();
-            SqlSession sqlss = sessionfact.openSession();
-            PacienteMapper pmapper=sqlss.getMapper(PacienteMapper.class);
-            List<Paciente> pacientes=pmapper.loadPacientes();
+        //Consulta con=new Consulta();
+        Eps ep=new Eps();        
+        Paciente pa= new Paciente(9888999,"CC","Oscar", Date.valueOf("2016-12-12"),ep);
+        SqlSessionFactory sessionfact = getSqlSessionFactory();
+        SqlSession sqlss = sessionfact.openSession();
+        PacienteMapper pmapper=sqlss.getMapper(PacienteMapper.class);
 
-            registrarNuevoPaciente(pmapper, pa);
-            conn.commit();
-            
-            /**
-            //pmapper.insertarPaciente(pa);
-            for (int i=0; i<pacientes.size(); i++){
-                Paciente re=pacientes.get(i);
-                System.out.print("miremos"+re.getNombre());
-            }
-             */
-            }catch(Exception e){}
-
-
-    }
-
+        registrarNuevoPaciente(pmapper, pa);
+        pmapper.insertarPaciente(pa);
+        List<Paciente> pacientes=pmapper.loadPacientes();
+        for (int i=0; i<pacientes.size(); i++){
+            Paciente re=pacientes.get(i);
+            System.out.print("miremos"+re.getNombre());
+        }
+        sqlSession.commit();        
+                //conn.close();         
+        }
     /**
      * Registra un nuevo paciente y sus respectivas consultas (si existiesen).
      * @param pmap mapper a traves del cual se hará la operacion
@@ -107,8 +89,8 @@ public class MyBATISExample {
         Consulta co=new Consulta(Date.valueOf("2016-12-12"),"Esta con mamoneria",1234);
         pmap.insertarPaciente(p);
         
-        pmap.insertConsulta(co, 9999, "CC", 90786);
-        
+        //pmap.insertConsulta(co, 9999, "CC", 90786);
+
         
     }
     
